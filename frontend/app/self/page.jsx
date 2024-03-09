99f8b74e33507b931487950c655d5bbf1847fda0
Utkarsh Saxena <utkarsh.saxena.2004@gmail.com>
2024-03-10 04:46:14 +0530

    Updated UI

--- frontend/app/self/page.jsx
+++ frontend/app/self/page.jsx
@@ -3,20 +3,39 @@ import React from "react";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import PomodoroTimer from "@/components/Pomodoro";
 import Todo from "@/components/Todo";
-import {Tabs, Tab, Card, CardBody} from "@nextui-org/react";
+import { Tabs, Tab, Card, CardBody } from "@nextui-org/react";
 import Board from "@/components/Board";
 
-
-function page() {
+function Page() {
   return (
-    //  <section className="flex w-full h-full">
-    //   <div className="h-full flex flex-col">
-
-    //   </div>
-
-    //  </section>
-
     <PanelGroup direction="horizontal" id="group" className="gap-2">
+      <Panel id="right-panel" defaultSize={80}>
+        <div className="flex w-full h-full flex-col">
+          <Tabs aria-label="Options" className="shadow-none">
+            <Tab key="music" title="Music" className="h-full">
+              <Card className="shadow-none h-full">
+                <CardBody>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </CardBody>
+              </Card>  
+            </Tab>
+            <Tab key="jamboard" title="Jamboard" className="h-full">
+              <Board />
+            </Tab>
+            <Tab key="resources" title="Resources" className="h-full">
+              <Card className="shadow-none h-full">
+                <CardBody>
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </CardBody>
+              </Card>  
+            </Tab>
+          </Tabs>
+        </div>  
+      </Panel>
+      <PanelResizeHandle
+        id="resize-handle"
+        className="bg-gray-200 w-1 h-20 my-auto rounded"
+      />
       <Panel
         id="left-panel"
         defaultSize={20}
@@ -26,40 +45,10 @@ function page() {
         className="flex flex-col gap-8"
       >
         <PomodoroTimer />
-        <Todo/>
-      </Panel>
-      <PanelResizeHandle
-        id="resize-handle"
-        className=" bg-gray-200 w-1 h-20 my-auto rounded"
-      />
-      <Panel id="right-panel" defaultSize={80}>
-        
-
-      <div className="flex w-full h-full flex-col">
-      <Tabs aria-label="Options" className="shadow-none relative">
-        <Tab key="music" title="Music" className="h-full">
-          <Card className="shadow-none h-full absolute left-0">
-            {console.log("Card")}
-            <CardBody>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </CardBody>
-          </Card>  
-        </Tab>
-        <Tab key="jamboard" title="Jamboard" className="h-full">
-          <Board/>
-        </Tab>
-        <Tab key="resources" title="Resources" className="h-full">
-          <Card className="shadow-none h-full">
-            <CardBody>
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </CardBody>
-          </Card>  
-        </Tab>
-      </Tabs>
-    </div>  
+        <Todo style={{ margin: "8px 0" }} />
       </Panel>
     </PanelGroup>
   );
 }
 
-export default page;
+export default Page;
