6edf9bac8fc3af544beb8e341d0d40897587a42f
techakhil-me <techakhilc47@gmail.com>
2024-03-09 23:00:50 +0530

    self added todo

--- frontend/app/self/page.jsx
+++ frontend/app/self/page.jsx
@@ -1,17 +1,37 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
-import {Card, CardHeader, CardBody, CardFooter} from "@nextui-org/react";
+import PomodoroTimer from "@/components/Pomodoro";
+import Todo from "@/components/Todo";
 
 function page() {
   return (
-   <section className="flex w-full h-full">
-    <div className="h-full flex flex-col">
+    //  <section className="flex w-full h-full">
+    //   <div className="h-full flex flex-col">
 
-    </div>
+    //   </div>
 
-   </section>
-  )
+    //  </section>
+
+    <PanelGroup direction="horizontal" id="group" className="gap-2">
+      <Panel
+        id="left-panel"
+        defaultSize={20}
+        minSize={12}
+        maxSize={20}
+        collapsible
+        className="flex flex-col gap-8"
+      >
+        <PomodoroTimer />
+        <Todo/>
+      </Panel>
+      <PanelResizeHandle
+        id="resize-handle"
+        className=" bg-gray-200 w-1 h-20 my-auto rounded"
+      />
+      <Panel id="right-panel" defaultSize={80}>right</Panel>
+    </PanelGroup>
+  );
 }
 
-export default page
+export default page;
